13425374c16eb80e9ed95b0c59b16bff2a352d0c
冯振威 <1181399414@qq.com>
2022-07-09 15:06:38 +0800

    🐛

--- packages/ws-ui/components/button/index.tsx
+++ packages/ws-ui/components/button/index.tsx
@@ -2,13 +2,13 @@
  * @Author: 冯振威 1181399414@qq.com
  * @Date: 2022-07-09 09:21:55
  * @LastEditors: 冯振威 1181399414@qq.com
- * @LastEditTime: 2022-07-09 11:55:02
+ * @LastEditTime: 2022-07-09 15:04:35
  * @FilePath: /ws-ui/packages/ws-ui/components/button/index.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 import Button from './button'
 
-export {SizeType as ButtonSize} from '../config-provider/SizeContext'
-export {ButtonProps, ButtonShape, ButtonType} from './button'
+export type {SizeType as ButtonSize} from '../config-provider/SizeContext'
+export type {ButtonProps, ButtonShape, ButtonType} from './button'
 
 export default Button
